0f620fa268728d24385c376f01b97cd90376bfd8
Michael Ieti <m.ieti001@gmail.com>
2016-10-01 00:45:02 +1300

    Adds an addPost function, which adds a placeholder post to the posts list.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = angular.module('flapperNews', [])
 
+//the scope variable is the bridge between Angular controllers
+//and Angular templates (i.e. the HTML thing with the extra markup)
+//Bind stuff to scope if you want it to be accessible in the template
+//e.g. a function or a variable.
 app.controller('MainCtrl', [
     '$scope',
     function($scope){
@@ -11,6 +15,11 @@ app.controller('MainCtrl', [
             {title: 'post 4', upvotes: 9},
             {title: 'post 5', upvotes: 4}
         ];
+        // omg functions as first class citizens!!! javascript ftw
+        // adds a new post to the posts list.
+        $scope.addPost = function(){
+            $scope.posts.push({title: 'A new post!', upvotes: 0});
+        }
     }
 ]);
 
